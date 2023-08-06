63d829bf40c8c85c9a3c41717a3324fd89c5fe29
Luan Araujo <luaanscar@gmail.com>
2023-08-06 18:19:45 -0300

    should be able to create a tweet

--- tests/Feature/TweetTest.php
+++ tests/Feature/TweetTest.php
@@ -1,26 +1,27 @@
 <?php
 
+use App\Http\Livewire\Tweet;
 use App\Models\User;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Livewire\livewire;
 
-it('should be able to create a tweet', function ($tweet) {
+it('should be able to create a tweet', function () {
     $user = User::factory()->create();
 
     actingAs($user);
 
-    livewire(Create::class)
-        ->set('body', $tweet)
+    livewire(Tweet\Create::class)
+        ->set('body', 'This is my first tweet')
         ->call('tweet')
         ->assertEmitted('tweet::created');
 
     assertDatabaseCount('tweets', 1);
 
-    expect(Tweet::first())
-        ->body->toBe($tweet)
+    expect(\App\Models\Tweet::first())
+        ->body->toBe('This is my first tweet')
         ->created_by->toBe($user->id);
-})->with(['my first tweet', 'my second tweet', 'my third tweet']);
+});
 
 // todo('body is required');
 // todo('the tweet body should have a max length of 140 characters');
